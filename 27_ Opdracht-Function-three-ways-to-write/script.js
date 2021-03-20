96105dc94d066d5491a9068ce518ff034cbf6290
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-03-20 11:16:51 +0100

    Add files via upload

---
+++ 27_ Opdracht-Function-three-ways-to-write/script.js
@@ -0,0 +1,29 @@
+/*function square(number1, number2) {
+  const multi = number1 * number1;
+  const multi2 = number2 * number2;
+  const add = multi + multi2;
+  const multi3 = add * add;
+  return multi3;
+}
+
+console.log(square(5, 6));*/
+
+/* const square = function (number1, number2) {
+  const multi = number1 * number1;
+  const multi2 = number2 * number2;
+  const add = multi + multi2;
+  const multi3 = add * add;
+  return multi3;
+};
+
+console.log(square(5, 6)); */
+
+const square = (number1, number2) => {
+  const multi = number1 * number1;
+  const multi2 = number2 * number2;
+  const add = multi + multi2;
+  const multi3 = add * add;
+  return multi3;
+};
+
+console.log(square(5, 6));
